ed437829699dc1b13f4d72e61ae0c59b7aa7a179
Oleg Bukovskiy <olbukovskiy@gmail.com>
2022-09-26 22:26:31 +0300

    change modal

--- modal.js
+++ modal.js
@@ -5,11 +5,31 @@
     modal: document.querySelector("[data-modal]"),
   };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtn.addEventListener("click", toggleModalRemove);
+  refs.closeModalBtn.addEventListener("click", toggleModalAdd);
+  refs.modal.addEventListener("click", onBackdropClick);
 
-  function toggleModal() {
-    document.body.classList.toggle("modal-open");
-    refs.modal.classList.toggle("is-hidden");
+  function toggleModalAdd() {
+    window.removeEventListener("keydown", onEscapeBtn);
+    document.body.classList.remove("modal-open");
+    refs.modal.classList.add("is-hidden");
+  }
+
+  function toggleModalRemove() {
+    window.addEventListener("keydown", onEscapeBtn);
+    document.body.classList.add("modal-open");
+    refs.modal.classList.remove("is-hidden");
+  }
+
+  function onBackdropClick(event) {
+    if (event.currentTarget === event.target) {
+      toggleModalAdd();
+    }
+  }
+
+  function onEscapeBtn(event) {
+    if (event.code === "Escape") {
+      toggleModalAdd();
+    }
   }
 })();
